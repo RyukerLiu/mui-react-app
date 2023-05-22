6aede75b6a071cc9c1ffafd169991b3a654dcb18
Allen Liu <alex03108861@gmail.com>
2023-05-22 18:50:11 +0800

    MUI React App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from "react";
+import Button from "@mui/material/Button";
+import CssBaseline from '@mui/material/CssBaseline';
+import SignIn from './SignIn';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
+function MyApp() {
+  return <Button variant="contained">Hello World</Button>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <MyApp />
+      <SignIn />
+    </React.Fragment>
   );
 }
 
