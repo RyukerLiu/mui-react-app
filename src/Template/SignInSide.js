371e166a231673b3333ba2e57c4ec7349fb9196d
Allen Liu <alex03108861@gmail.com>
2023-05-29 18:37:29 +0800

    Responsive

--- src/Template/SignInSide.js
+++ src/Template/SignInSide.js
@@ -54,7 +54,7 @@ export default function SignInSide() {
                 <Grid
                     item
                     xs={false}
-                    sm={4}
+                    sm={6}
                     md={7}
                     sx={{
                         backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
@@ -65,7 +65,7 @@ export default function SignInSide() {
                         backgroundPosition: 'center',
                     }}
                 />
-                <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                <Grid item xs={12} sm={6} md={5} component={Paper} elevation={6} square>
                     <Box
                         sx={{
                             my: 8,
@@ -75,7 +75,7 @@ export default function SignInSide() {
                             alignItems: 'center',
                         }}
                     >
-                        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
                             <LockOutlinedIcon />
                         </Avatar>
                         <Typography component="h1" variant="h5">
