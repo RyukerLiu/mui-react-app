f50105f59eb73341803c77c8d5a1f951e93a7ea8
Allen Liu <alex03108861@gmail.com>
2023-05-29 15:16:28 +0800

    Side

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import * as React from "react";
 import CssBaseline from '@mui/material/CssBaseline';
-import SignIn from './SignIn';
+import SignInSide from './Template/SignInSide';
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -13,7 +13,7 @@ function App() {
   return (
     <React.Fragment>
       <CssBaseline />
-      <SignIn />
+      <SignInSide />
     </React.Fragment>
   );
 }
